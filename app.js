2dc12cb2973fcfe99586a198186b5cd5d2cfe187
BRECHT-PC\brecht <brecht.valcke@gentwebart.be>
2017-03-30 13:15:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+var fs = require("fs");
+
+io.on("connection", function(client) {
+    client.emit('connected', {
+        connected: true
+    });
+
+    client.on("setUsername", function(name) {
+      if(client.username!==undefined){
+        // stuurt disconnect bij namechange omdat het zogezegt nieuwe user is en zodat elke client naam verwijderd.
+        client.broadcast.emit("userDisconnected", client.username);
+      }
+        client.username = name;
+        client.emit("recievedUsername", client.username);
+        client.broadcast.emit("userConnected", client.username);
+        console.log(client.username + ' connected');
+    });
+    client.on("getAllConnected", function() {
+      var clients = io.sockets.connected;
+      var clientNames = [];
+      for (var clientId in clients){
+        var clientObj=io.sockets.connected[clientId];
+        clientNames.push(clientObj.username);
+      }
+      client.emit("RecieveAllConnected",clientNames);
+
+    });
+    client.on('disconnect', function(){
+      console.log(client.username + ' disconnected');
+      client.broadcast.emit("userDisconnected", client.username);
+  });
+});
+app.get("*", function(req, res) {
+    fs.createReadStream("./public/index.html").pipe(res);
+});
+server.listen(80);
